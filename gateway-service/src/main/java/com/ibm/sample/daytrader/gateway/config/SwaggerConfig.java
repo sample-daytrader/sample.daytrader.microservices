153baa67cf59b799d03fc35bd321f7ec8f9c4eba
Rahul Krishna <rkrsn@ibm.com>
2022-12-18 14:18:07 -0800

    Migrated gateway service to spring 2.7.6

--- gateway-service/src/main/java/com/ibm/sample/daytrader/gateway/config/SwaggerConfig.java
+++ gateway-service/src/main/java/com/ibm/sample/daytrader/gateway/config/SwaggerConfig.java
@@ -19,24 +19,73 @@ package com.ibm.sample.daytrader.gateway.config;
 
 import com.google.common.base.Predicates;
 
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
+import org.springframework.boot.actuate.endpoint.ExposableEndpoint;
+import org.springframework.boot.actuate.endpoint.web.*;
+import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {                                    
+public class SwaggerConfig {
+    private WebEndpointsSupplier webEndpointsSupplier;
+    private ServletEndpointsSupplier servletEndpointsSupplier;
+    private ControllerEndpointsSupplier controllerEndpointsSupplier;
+    private EndpointMediaTypes endpointMediaTypes;
+    private CorsEndpointProperties corsProperties;
+    private WebEndpointProperties webEndpointProperties;
+    private Environment environment;
+
     @Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())                                     
-          .paths(Predicates.not(PathSelectors.regex("/error"))) // exclude Spring error controller
-          .build();                                           
+    public Docket api() {
+        Docket build = new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error"))) // exclude Spring error controller
+                .build();
+        return build;
+    }
+
+    @Bean
+    public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier, ServletEndpointsSupplier servletEndpointsSupplier, ControllerEndpointsSupplier controllerEndpointsSupplier, EndpointMediaTypes endpointMediaTypes, CorsEndpointProperties corsProperties, WebEndpointProperties webEndpointProperties, Environment environment) {
+        this.webEndpointsSupplier = webEndpointsSupplier;
+        this.servletEndpointsSupplier = servletEndpointsSupplier;
+        this.controllerEndpointsSupplier = controllerEndpointsSupplier;
+        this.endpointMediaTypes = endpointMediaTypes;
+        this.corsProperties = corsProperties;
+        this.webEndpointProperties = webEndpointProperties;
+        this.environment = environment;
+        List<ExposableEndpoint<?>> allEndpoints = new ArrayList();
+        Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
+        allEndpoints.addAll(webEndpoints);
+        allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
+        allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
+        String basePath = webEndpointProperties.getBasePath();
+        EndpointMapping endpointMapping = new EndpointMapping(basePath);
+        boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(webEndpointProperties, environment, basePath);
+        WebMvcEndpointHandlerMapping webMvcEndpointHandlerMapping = new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes, corsProperties.toCorsConfiguration(), new EndpointLinksResolver(allEndpoints, basePath), shouldRegisterLinksMapping, null);
+        return webMvcEndpointHandlerMapping;
+    }
+
+
+    private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties, Environment environment, String basePath) {
+        return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath) || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
     }
 }
